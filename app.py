f95a2d0a57139a863f072cb19b3b7e5a3985fe14
Martin Rauscher <martin@ultimatetournament.io>
2023-02-03 10:03:09 +0100

    convert result image

--- app.py
+++ app.py
@@ -104,11 +104,38 @@ def inference(model_inputs:dict) -> dict:
     print(f"finished ldm stable in {(time.monotonic_ns() - ldmStart)/1_000_000_000}s")
 
     buffered = BytesIO()
-    images[0].save(buffered,format="PNG")
+    conv_images(images)[0].save(buffered,format="PNG")
     image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
     # Return the results as a dictionary
     return {'image_base64': image_base64}
+
+
+def conv_images(images, num_rows=1, offset_ratio=0.02):
+    if type(images) is list:
+        num_empty = len(images) % num_rows
+    elif images.ndim == 4:
+        num_empty = images.shape[0] % num_rows
+    else:
+        images = [images]
+        num_empty = 0
+
+    empty_images = np.ones(images[0].shape, dtype=np.uint8) * 255
+    images = [image.astype(np.uint8) for image in images] + [empty_images] * num_empty
+    num_items = len(images)
+
+    h, w, c = images[0].shape
+    offset = int(h * offset_ratio)
+    num_cols = num_items // num_rows
+    image_ = np.ones((h * num_rows + offset * (num_rows - 1),
+                      w * num_cols + offset * (num_cols - 1), 3), dtype=np.uint8) * 255
+    for i in range(num_rows):
+        for j in range(num_cols):
+            image_[i * (h + offset): i * (h + offset) + h:, j * (w + offset): j * (w + offset) + w] = images[
+                i * num_cols + j]
+
+    pil_img = Image.fromarray(image_)
+    return pil_img
 
 class LocalBlend:
     
